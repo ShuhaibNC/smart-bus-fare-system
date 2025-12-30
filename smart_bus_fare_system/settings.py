f161d79198a2b1395e73456966580bf1467f296f
Azad Rajeev <azadrajeev1123@gmail.com>
2025-12-31 01:00:18 +0530

    v4.6

--- smart_bus_fare_system/settings.py
+++ smart_bus_fare_system/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sbfs',    # from phpMyAdmin
         'USER': 'root',   # from phpMyAdmin
-        'PASSWORD': 'cms123', 
+        'PASSWORD': '123', 
         'HOST': 'localhost',       # usually localhost
         'PORT': '3306',            # default MySQL port
     }
