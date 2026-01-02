49634818dcf03c657a7d49795485a4124eca5473
Azad Rajeev <azadrajeev1123@gmail.com>
2026-01-03 02:17:54 +0530

    v5.6

--- student/urls.py
+++ student/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.student_login, name='student_login'),
-    path('logout/', views.student_logout, name='logout'),
+    path('block_card', views.block_card, name='block_card'),
+
+    
 ]
